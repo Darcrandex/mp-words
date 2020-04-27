c037648223719e81d2090b9ff9638e24bd5259ac
darcrand <darcrandex@gmail.com>
2020-04-27 23:04:14 +0800

    完成关于页面

--- client/src/pages/about/index.tsx
+++ client/src/pages/about/index.tsx
@@ -1,18 +1,21 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text, Image } from '@tarojs/components'
+import Taro, { Component, Config } from "@tarojs/taro"
+import { View, Text, Image } from "@tarojs/components"
 
-import './styles.less'
+import logo from "@/assets/images/words-logo.png"
+import "./styles.less"
 
 export default class About extends Component {
   config: Config = {
-    navigationBarTitleText: 'About',
+    navigationBarTitleText: "About"
   }
 
   render() {
     return (
       <View className="container">
-        <Image src="" className="logo" />
-        <Text>Just Words</Text>
+        <Image src={logo} className="logo" />
+        <Text className="title">Just Words</Text>
+        <Text className="sub_text">There's always some words that moves you.</Text>
+        <Text className="sub_text">made by Darcrand</Text>
       </View>
     )
   }
